0f6ee33e1faa6002373b3f25c09584cbda0682dd
SCHULTHEISS Robert <robert.schultheiss@soprasteria.com>
2020-10-28 18:13:34 +0100

    routing added to video components

--- src/app/body/bodyParts/body-parts.module.ts
+++ src/app/body/bodyParts/body-parts.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from "@angular/common";
 import { VideoCountCarComponent } from "./video-count-car/video-count-car.component";
 import { VideoCountPeopleComponent } from "./video-count-people/video-count-people.component";
 import { VideoCountUmbrellaComponent } from "./video-count-umbrella/video-count-umbrella.component";
+import { BodyPartsRoutingModule } from "./body-parts-routing.module";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, BodyPartsRoutingModule
+  ],
   declarations: [
     VideoCountCarComponent,
     VideoCountPeopleComponent,
